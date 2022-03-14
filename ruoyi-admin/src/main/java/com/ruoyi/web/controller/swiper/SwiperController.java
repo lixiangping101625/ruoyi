3afd8e64f7d76ac21f8680aa9348acf5bb744822
lixiangping <admin>
2022-03-14 13:54:48 +0800

    commit

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/swiper/SwiperController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/swiper/SwiperController.java
@@ -29,7 +29,7 @@ import com.ruoyi.common.core.page.TableDataInfo;
  * @date 2022-03-10
  */
 @RestController
-@RequestMapping("/system/swiper")
+@RequestMapping("/swiper")
 public class SwiperController extends BaseController
 {
     @Autowired
