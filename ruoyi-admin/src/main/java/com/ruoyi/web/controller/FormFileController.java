eb5e25331a6f131559999d127234dac311618daa
lixiangping <admin>
2022-03-19 13:15:39 +0800

    文件上传

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/FormFileController.java
@@ -0,0 +1,54 @@
+package com.ruoyi.web.controller;
+
+import com.sun.javafx.binding.StringFormatter;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 表单文件上传
+ */
+@RestController
+@RequestMapping("/file")
+public class FormFileController {
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("email") String email,
+                         @RequestParam("username") String username,
+                         @RequestPart("headerImg") MultipartFile headerImg) {
+//                         @RequestPart("photos") MultipartFile[] photos) {
+
+        System.out.println(StringFormatter.concat("上传的信息：email={},username={},headerImg={}",
+                email, username, headerImg.getSize()));
+        if (!headerImg.isEmpty()) {
+            //保存文件到服务器
+            String originalFilename = headerImg.getOriginalFilename();
+            try {
+                File file = new File("H:\\spring_boot" + File.separator + "file" + File.separator + originalFilename);
+                if (!file.exists()) {
+                    file.mkdirs();
+                }
+                headerImg.transferTo(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+//        if (photos.length > 0) {
+//            for (MultipartFile multipartFile : photos) {
+//                System.out.println(multipartFile.getOriginalFilename());
+//                try {
+//                    String originalFilename = multipartFile.getOriginalFilename();
+//                    multipartFile.transferTo(new File("D:\\spring_annation\\file\\" + originalFilename));
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+        return "上传成功";
+    }
+
+
+}
